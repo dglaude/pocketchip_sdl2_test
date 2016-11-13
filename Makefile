0af07852886921643563b59725fe3807f1c682ac
Simon Bates <simon@bitstructures.com>
2016-11-13 18:55:46 -0500

    Init

---
+++ Makefile
@@ -0,0 +1,2 @@
+pocketchip_sdl2_test: main.c
+	gcc -o pocketchip_sdl2_test -l SDL2 -Wpedantic -Wall main.c
---
+++ main.c
@@ -0,0 +1,9 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+int
+main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    return 0;
+}
